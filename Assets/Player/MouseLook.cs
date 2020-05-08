640e799b9cc080a98ec8b7e1485d528179974aee
JEFFe <andrejeff.leevi@gmail.com>
2020-05-08 15:14:08 +0300

    Initial push
    tavaraa on paljon

---
+++ Assets/Player/MouseLook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensivity = 200f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        } else if (Cursor.lockState == CursorLockMode.None)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        float mouseX = Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
