ea06dc5bbeabfef17960a9a4842be392708961dc
JEFFe <andrejeff.leevi@gmail.com>
2020-05-08 17:52:50 +0300

    Epic

--- Assets/Player/MovementScript.cs
+++ Assets/Player/MovementScript.cs
@@ -6,9 +6,9 @@ public class MovementScript : MonoBehaviour
 {
 
     public CharacterController controller;
-    public float speed = 12f;
+    public float speed = 8f;
     public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    public float jumpHeight = 2f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -45,8 +45,48 @@ public class MovementScript : MonoBehaviour
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
+
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    public float friction = 3f;
+    public float ground_accelerate = 6f;
+
+    public float max_velocity_ground = 300f;
+    public float air_accelerate = 12f;
+    public float max_velocity_air = 300f;
+
+    private Vector3 Accelerate(Vector3 accelDir, Vector3 prevVelocity, float accelerate, float max_velocity)
+    {
+        float projVel = Vector3.Dot(prevVelocity, accelDir); // Vector projection of Current velocity onto accelDir.
+        float accelVel = accelerate * Time.fixedDeltaTime; // Accelerated velocity in direction of movment
+
+        // If necessary, truncate the accelerated velocity so the vector projection does not exceed max_velocity
+        if (projVel + accelVel > max_velocity)
+            accelVel = max_velocity - projVel;
+
+        return prevVelocity + accelDir * accelVel;
+    }
+
+    private Vector3 MoveGround(Vector3 accelDir, Vector3 prevVelocity)
+    {
+        // Apply Friction
+        float speed = prevVelocity.magnitude;
+        if (speed != 0) // To avoid divide by zero errors
+        {
+            float drop = speed * friction * Time.fixedDeltaTime;
+            prevVelocity *= Mathf.Max(speed - drop, 0) / speed; // Scale the velocity based on friction.
+        }
+
+        // ground_accelerate and max_velocity_ground are server-defined movement variables
+        return Accelerate(accelDir, prevVelocity, ground_accelerate, max_velocity_ground);
+    }
+
+    private Vector3 MoveAir(Vector3 accelDir, Vector3 prevVelocity)
+    {
+        // air_accelerate and max_velocity_air are server-defined movement variables
+        return Accelerate(accelDir, prevVelocity, air_accelerate, max_velocity_air);
     }
 }
